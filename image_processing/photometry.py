105cd77c537ae23362971eafb053146950ccdce4
Wu Mingsong <mingsong123@gmail.com>
2021-10-16 18:17:17 +0100

    implemented circle outline generator. needs some streamlining

--- image_processing/photometry.py
+++ image_processing/photometry.py
@@ -6,36 +6,35 @@ import sys
 from numba import njit
 
 
-### sets gray values from left and right edges to 0 (blackest black)
-### outputs a mask
-def del_grays(image, grayvalue):
 
-    mask = np.ones(np.shape(image))
-    for index, line in enumerate(image):
-        if line[0] == grayvalue:
-            mask[index][0] = 0
-            runner = 1
-            while runner < len(line) and line[0+runner] == grayvalue:
-                mask[index][0+runner] = 0
-                runner += 1
+def circle(centre, radius):
 
-        if line[-1] == grayvalue:
-            mask[index][-1] = 0
-            runner = 1
-            while runner < len(line) and line[-(1+runner)] == grayvalue:
-                mask[index][-(1+runner)] = 0
-                runner += 1
-    return mask
+    first_oct = [[], []]
 
-@njit
-def upper_threshold(image, threshold):
+    initial = [centre[0] + radius, centre[1]]
+    next = [0, 0]
+    # print(round(centre[1] + radius/np.sqrt(2)))
+    while next[1] < centre[1] + radius/np.sqrt(2) - 1:
 
-    mask = np.ones(np.shape(image))
-    coordinates = np.where(image > threshold)
-    for i in range(len(coordinates[0])):
+        next_x2 = initial[0]**2 - 2*initial[1] - 1
+        next_y2 = radius**2 - next_x2
+        next = [np.sqrt(next_x2), np.sqrt(next_y2)]
+        # print(next)
+        for i in range(2): first_oct[i].append(round(next[i]))
+        initial = next
 
-        x = coordinates[0][i]
-        y = coordinates[1][i]
-        mask[x][y] = 0
 
-    return mask
+    first_quad = [first_oct[0] + list(reversed(first_oct[1])), first_oct[1] + list(reversed(first_oct[0]))]
+    right_hemi = [list(reversed(first_quad[0])) + first_quad[0], list(reversed(first_quad[1])) + [-1 * i for i in first_quad[1]]]
+    left_hemi = [[-1 * i for i in right_hemi[0]], right_hemi[1]]
+
+    return right_hemi, left_hemi
+
+
+right_hemi, left_hemi = circle([0, 0], 23)
+print(right_hemi)
+dummy = [[], []]
+# for index, y in right_hemi[1]:
+#
+#     dummy[1].append(y)
+#     if right_hemi[]
